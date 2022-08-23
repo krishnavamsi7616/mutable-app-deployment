b3ba1e1868c8b95ffbb3fd092bb6ee2e73489345
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-23 16:16:40 -0700

    updating ./mutable-app-deployment

--- deploy.tf
+++ deploy.tf
@@ -2,12 +2,12 @@ resource "null_resource" "ansible" {
   triggers = {
     abc = timestamp()
   }
-  count        =
+  count        = ""
   provisioner "remote-exec" {
     connection {
       user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
       password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
-      host     = aws_instance.instance.*.private_ip[count.index]
+      host     = ""
     }
     inline = [
       "ansible-pull -U https://github.com/krishnavamsi7616/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=${var.ENV} -e DOCDB_ENDPOINT=${data.terraform_remote_state.mutable.outputs.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${data.terraform_remote_state.mutable.outputs.REDIS_ENDPOINT} -e MYSQL_ENDPOINT=${data.terraform_remote_state.mutable.outputs.MYSQL_ENDPOINT}",
